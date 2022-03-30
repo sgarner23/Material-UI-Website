fec54462fb442b1521a29a4e4920deb294299df6
Stephen Garner <stephenallen.garner@gmail.com>
2022-03-30 17:53:04 -0600

    landing page animation created

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Footer from "./ui/Footer";
 
 import theme from "./ui/Theme";
+import LandingPage from "./LandingPage";
 
 function App() {
   const [selectedIndex, setSelectedIndex] = useState(0)
@@ -15,7 +16,7 @@ function App() {
       <BrowserRouter>
         <Header value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
         <Switch>
-          <Route exact path="/" component={() => <div style={{ height: '2000px' }}>Home</div>} />
+          <Route exact path="/" component={LandingPage} />
           <Route exact path="/services" component={() => <div>Services</div>} />
           <Route exact path="/customsoftware" component={() => <div>Custom Software</div>} />
           <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>} />
